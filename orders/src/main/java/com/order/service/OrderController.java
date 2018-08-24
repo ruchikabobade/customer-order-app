bdb21b5fcd4fd06d94a6edec7f4fb215bc0d60d1
ruchikabobade <40056438+ruchikabobade@users.noreply.github.com>
2018-08-24 11:44:43 +0530

    Update OrderController.java

--- orders/src/main/java/com/order/service/OrderController.java
+++ orders/src/main/java/com/order/service/OrderController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = {"*"})
 @RequestMapping("/orders")
 public class OrderController {
 	
